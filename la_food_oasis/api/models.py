20b95a2252cb01a645ac7d5fe0438eec00859fe1
michaelkunc <mjkunc@gmail.com>
2016-08-20 18:19:43 -0700

    added lat and long to Location model

--- la_food_oasis/api/models.py
+++ la_food_oasis/api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Location(models.Model):
-    LOCATION_TYPES = ('Community Garden', "Farmer's Market", 'Food Bank', 'Grocery Store')
+    LOCATION_TYPES = ('Community Garden', "Farmer's Market",
+                      'Food Bank', 'Grocery Store')
 
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=80, blank=False)
@@ -12,6 +13,8 @@ class Location(models.Model):
     state = models.CharField(max_length=2, blank=True, default='CA')
     zip = models.CharField(max_length=5, blank=False)
     phone = models.CharField(max_length=20, blank=False)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
     type = models.CharField(choices=LOCATION_TYPES, blank=False)
 
     class Meta:
