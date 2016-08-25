e9b2382ba68a62a720bfbc26218aa26d5454182e
michaelkunc <mjkunc@gmail.com>
2016-08-24 18:06:13 -0700

    basic view and urls complete

--- la_food_oasis/api/views.py
+++ la_food_oasis/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from api.models import Location
@@ -9,45 +9,45 @@ from api.serializers import LocationSerializer
 
 class JSONResponse(HttpResponse):
 
-    def __init__(self, data ** kwargs):
+    def __init__(self, data, **kwargs):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init(content, **kwargs)
+        super(JSONResponse, self).__init__(content, **kwargs)
 
-    @csrf_exempt
-    def location_list(request):
-        if request.method == 'GET':
-            locations = Location.objects.all()
-            serializer = LocationSerializer(locations, many=True)
-            return JSONResponse(serializer.data)
+@csrf_exempt
+def location_list(request):
+    if request.method == 'GET':
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return JSONResponse(serializer.data)
 
-        elif request.method == 'POST'
-            data = JSONParser().parse(request)
-            serializer = LocationSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return JSONResponse(serializer.data, status=201)
-            return JSONResponse(serializer.errors, status=400)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = LocationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
 
-    @csrf_exempt
-    def location_detail(request, pk):
-        try:
-            location = Location.objects.get(pk=pk)
-        except Location.DoesNotExist:
-            return HttpResponse(status=404)
+@csrf_exempt
+def location_detail(request, pk):
+    try:
+        location = Location.objects.get(pk=pk)
+    except Location.DoesNotExist:
+        return HttpResponse(status=404)
 
-        if request.method == 'GET':
-            serializer = LocationSerializer(location)
-            return JSONResponse(serializer.data)
+    if request.method == 'GET':
+        serializer = LocationSerializer(location)
+        return JSONResponse(serializer.data)
 
-        elif request.method == 'PUT':
-            data = JSONParser().parse(request)
-            serializer = LocationSerializer(location, data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return  JSONResponse(serializer.data)
-            return JSONResponse(serializer.errors, status=400)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LocationSerializer(location, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return  JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
 
-        elif request.method == 'DELETE':
-            location.delete()
-            return HttpResponse(status=204)
+    elif request.method == 'DELETE':
+        location.delete()
+        return HttpResponse(status=204)
