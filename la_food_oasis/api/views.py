d92d46188e764a28afec81be950eba6fa7a7a79b
michaelkunc <mjkunc@gmail.com>
2016-08-24 17:19:54 -0700

    start of location view

--- la_food_oasis/api/views.py
+++ la_food_oasis/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.models import Location
+from api.serializers import LocationSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+
+    def __init__(self, data **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init(content, **kwargs)
+
+    @csrf_exempt
+    def location_list(request):
+        if request.method == 'GET':
+            locations = Location.objects.all()
+            serializer = LocationSerializer(locations, many=True)
+            return JSONResponse(serializer.data)
+
+        elif request.method == 'POST'
+            data = JSONParser().parse(request)
+            serializer = LocationSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JSONResponse(serializer.data, status=201)
+            return JSONResponse(serializer.errors, status=400)
+
+    @csrf_exempt
+    def location_detail(request, pk):
