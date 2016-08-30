258061ded08604391adf6bc921e23e3fa949183c
kikisdeliveryservice <kikideliveryservice2013@gmail.com>
2016-08-29 21:47:48 -0700

    models.py: zip is a built in function for clarity -> zipcode

--- la_food_oasis/api/models.py
+++ la_food_oasis/api/models.py
@@ -10,7 +10,7 @@ class Location(models.Model):
     address_2 = models.CharField(max_length=40, blank=True, default='')
     city = models.CharField(max_length=30, blank=False)
     state = models.CharField(max_length=2, blank=True, default='CA')
-    zip = models.CharField(max_length=5, blank=False)
+    zipcode = models.CharField(max_length=5, blank=False)
     phone = models.CharField(max_length=20, blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
