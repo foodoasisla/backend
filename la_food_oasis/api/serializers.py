299ecf6405d06d5f4b0619162603d8a176a07cf0
michaelkunc <mjkunc@gmail.com>
2016-09-20 19:31:36 -0700

    updated requirements.txt and CONTRIBUTING.md

--- la_food_oasis/api/serializers.py
+++ la_food_oasis/api/serializers.py
@@ -7,7 +7,7 @@ class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ('id', 'name', 'address_1', 'address_2', 'city',
-                  'state', 'zip', 'phone', 'latitude', 'longitude', 'category')
+                  'state', 'zipcode', 'phone', 'latitude', 'longitude', 'category')
 
     def create(self, validated_data):
         return Location.objects.create(**validated_data)
@@ -20,7 +20,7 @@ class LocationSerializer(serializers.ModelSerializer):
             'address_2', instance.address_2)
         instance.city = validated_data.get('city', instance.city)
         instance.state = validated_data.get('state', instance.state)
-        instance.zip = validated_data.get('zip', instance.zip)
+        instance.zip = validated_data.get('zipcode', instance.zip)
         instance.phone = validated_data.get('phone', instance.phone)
         instance.latitude = validated_data.get('latitude', instance.latitude)
         instance.longitude = validated_data.get('longitude', instance.longitude)
