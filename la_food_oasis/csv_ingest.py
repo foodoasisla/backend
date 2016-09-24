6d440e0fe72565a9d028f89ec04820d4f690999e
michaelkunc <mjkunc@gmail.com>
2016-09-24 08:40:08 -0700

    modification to ingest file

--- la_food_oasis/csv_ingest.py
+++ la_food_oasis/csv_ingest.py
@@ -1,10 +1,12 @@
 import csv
 from api.models import Location
+import os
 
 
-with open("communitygardens.csv", 'r') as f:
-    reader = csv.reader(f)
-    next(f) #skips first line so we don't import headers
-    for r in reader:
-        loc = Location(name=r[1], category=r[2],address_1=r[3], city=r[4], state=r[5],zipcode=r[6], latitude=float(r[7]), longitude=float(r[8]))
-        loc.save()
+for filename in os.listdir('csv_files'):
+    with open(os.path.join('csv_files', filename), 'r') as f:
+        reader = csv.reader(f)
+        next(f) #skips first line so we don't import headers
+        for r in reader:
+            loc = Location(name=r[1], category=r[2],address_1=r[3], city=r[4], state=r[5],zipcode=r[6], latitude=float(r[7]), longitude=float(r[8]))
+            loc.save()
