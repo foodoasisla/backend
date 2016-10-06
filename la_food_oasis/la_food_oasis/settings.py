99902c2afa125332e00a9505fb4ca4b8e9ef2cd1
michaelkunc <mjkunc@gmail.com>
2016-10-06 14:56:37 -0700

    fix settings.py

--- la_food_oasis/la_food_oasis/settings.py
+++ la_food_oasis/la_food_oasis/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/home/django/django_project/django_project/static'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
