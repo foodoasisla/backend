24c0ea79503fd1c542037c5ec0b93cce5f439ff4
michaelkunc <mjkunc@gmail.com>
2016-10-06 14:59:34 -0700

    more changes to the settings.py

--- la_food_oasis/la_food_oasis/settings.py
+++ la_food_oasis/la_food_oasis/settings.py
@@ -131,5 +131,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
