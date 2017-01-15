7e928c29cf8acf56d108a68e13fc96a04af66f1c
michaelkunc <mjkunc@gmail.com>
2017-01-15 13:44:22 -0800

    added django_nose back to requirements

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 import psycopg2
+import django_nose
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
