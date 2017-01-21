80b79f50e959410a34b614b11b65433876c60ffe
michaelkunc <mjkunc@gmail.com>
2017-01-21 13:46:20 -0800

    packaging up static files

--- backend/settings.py
+++ backend/settings.py
@@ -21,6 +21,8 @@ import urllib.parse as urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static'
 
 
 # Quick-start development settings - unsuitable for production
