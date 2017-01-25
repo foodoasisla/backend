16289a39bca240cea55f1d29c61499710d5bb4c6
michaelkunc <mjkunc@gmail.com>
2017-01-24 20:13:12 -0800

    add django-extensions to installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
-    'django_nose'
+    'django_nose',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
