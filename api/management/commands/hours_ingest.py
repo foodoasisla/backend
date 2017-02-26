ab5c62fd366707e24f0ccc4e24df02cfcca76889
michaelkunc <mjkunc@gmail.com>
2017-02-26 14:07:15 -0800

    hours ingest working properly.

--- api/management/commands/hours_ingest.py
+++ api/management/commands/hours_ingest.py
@@ -23,16 +23,4 @@ class Command(BaseCommand):
                     for i in [r[11:i + 3] for i in range(11, len(r), 3)]:
                         h = Hour(day=i[0], open_time=i[1],
                                  close_time=i[2], location=loc)
-                        print(h)
                         h.save()
-
-
-# in progress ingest script
-# sample_data = ['Arleta Community Garden','8800 Canterbury Avenue',' ','San Fernando Valley','CA','91331',' ',34.22961903400005, -118.42451217799999,'Community Garden','Y','1stMo', '08:00', '12:00','2ndTu', '08:00', '12:00']
-#
-# create the location
-# loc = Location(name=sample_data[0],address_1=sample_data[1],address_2=sample_data[2],city=sample_data[3], state=sample_data[4], zipcode=sample_data[5], phone=sample_data[6], latitude=sample_data[7], longitude=sample_data[7],category='CG',active=True)
-# loc.save()
-# create the hour for the given location
-# for i in [sample_data[11:][i:i+3] for i in range(0, len(sample_data[11:-1]),3)]:
-    # h = Hour(day=i[0], open_time=i[1], close_time=i[2], location = loc)
