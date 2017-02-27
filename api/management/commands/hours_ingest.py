66d2801e5f3dee07ccb6d75fd902a5a692b1c9de
michaelkunc <mjkunc@gmail.com>
2017-02-26 17:02:16 -0800

    seems the hours ingest is working.

--- api/management/commands/hours_ingest.py
+++ api/management/commands/hours_ingest.py
@@ -20,7 +20,8 @@ class Command(BaseCommand):
                     loc = Location(name=r[0], address_1=r[1], address_2=r[2], city=r[3], state=r[
                                    4], zipcode=r[5], phone=r[6], latitude=r[7], longitude=r[8], category=r[9], website=r[10], active=True)
                     loc.save()
-                    for i in [r[11:i + 3] for i in range(11, len(r), 3)]:
+                    hours = list(filter(None, r[11:]))
+                    for i in [hours[i:i + 3] for i in range(0, len(hours), 3)]:
                         h = Hour(day=i[0], open_time=i[1],
                                  close_time=i[2], location=loc)
                         h.save()
