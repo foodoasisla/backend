7fd3bf77170fbef32f5c883768e0fae4feca80b7
michaelkunc <mjkunc@gmail.com>
2017-04-05 15:06:21 -0700

    ingest script now loads community gardens, food pantries, farmers markets and supermarkets

--- api/management/commands/csv_ingest.py
+++ api/management/commands/csv_ingest.py
@@ -1,21 +1,43 @@
 from django.core.management.base import BaseCommand
+from django.core import management
 import csv
+
 from api.models import Location, Hour
-import os
 
 
 class Command(BaseCommand):
-    help = 'Imports CSV file to populate local DB'
+    help = 'Ingests data into the db from csv_files. This command will kill and fill the db.'
 
     def handle(self, *args, **options):
-        for filename in ['farmers_markets_test_data.csv']:
-            print('importing CSV file {0}'.format(filename))
-            with open(os.path.join('csv_files', filename), 'r') as f:
-                reader = csv.reader(f)
-                next(f)  # skips first line so we don't import headers
-                for r in reader:
-                    print('populating row: {0}'.format(r))
-                    loc = Location(name=r[1], category=r[2], address_1=r[3],
-                                   city=r[4], state=r[5], zipcode=r[6],
-                                   latitude=float(r[7]), longitude=float(r[8]))
-                    loc.save()
+        # this does a full kill and fill on the database.
+        management.call_command('flush', verbosity=0, interactive=False)
+        csv_files = ['farmers-market.csv',
+                     'community_gardens.csv', 'food-pantry.csv', 'supermarket.csv']
+        location_count = 0
+        for file in csv_files:
+            try:
+                with open('csv_files/{}'.format(file), 'r') as f:
+                    reader = csv.reader(f)
+                    next(f)
+                    for r in reader:
+                        loc = Location(name=r[0], address_1=r[1], address_2=r[2], city=r[3], state=r[
+                                       4], zipcode=r[5], phone=r[6], latitude=r[7], longitude=r[8], category=r[9], website=r[10], active=True)
+                        loc.save()
+                        hours = list(filter(None, r[11:]))
+                        for i in [hours[i:i + 3] for i in range(0, len(hours), 3)]:
+                            h = Hour(day=i[0], open_time=self._format_time(i[1]),
+                                     close_time=self._format_time(i[2]), location=loc)
+                            h.save()
+                        location_count += 1
+
+            except:
+                self.stdout.write(
+                    "Not all rows loaded to the database. The last row loaded was {0} {1}. The ingest failed in file {2}".format(loc.name, loc.address_1, file))
+
+        self.stdout.write(
+            "{} locations have been added to the database.".format(location_count))
+
+    def _format_time(self, time):
+        time_list = list(time)
+        time_list.insert(-2, ':')
+        return ''.join(time_list)
