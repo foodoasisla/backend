12af3250860951c0802d2f692afaf12f692e7bb0
michaelkunc <mjkunc@gmail.com>
2017-06-22 14:31:35 -0700

    improve tests

--- api/tests/views_test.py
+++ api/tests/views_test.py
@@ -16,7 +16,7 @@ class TestViews(TestCase):
             latitude=34.0086873, longitude=-118.4033927,
             category='Grocery Store')
 
-    def test_fetches_index(self):
+    def test_index(self):
         response = self.client.get('/locations/')
         self.assertEqual(200, response.status_code)
         self.assertEqual(4, len(response.json()))
@@ -26,30 +26,32 @@ class TestViews(TestCase):
         self.assertEqual('Spring Street Community Garden',
                          response.json()['results'][0]['name'])
 
-    def test_fetches_by_location_within_radius(self):
+    def test_location_within_radius(self):
         response = self.client.get('/nearby_locations/', {'latitude': 34.0444447,
                                                           'longitude': -118.296597})
         self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.json()['count'])
         self.assertEqual('Spring Street Community Garden',
                          response.json()['results'][0]['name'])
 
-    def test_fetches_by_location_outside_of_radius(self):
+    def test_location_outside_of_radius(self):
         # Santa Monica State Beach
         response = self.client.get('/nearby_locations/', {'latitude': 34.0051628,
                                                           'longitude': -118.5145828})
         self.assertEqual(200, response.status_code)
         self.assertEqual([], response.json()['results'])
 
-    def test_fetches_by_location_with_large_radius_override(self):
+    def test_location_with_large_radius_override(self):
         # Baldwin Hills Scenic Overlook
         response = self.client.get('/nearby_locations/', {'radius': 15000,
                                                           'latitude': 34.0139142,
                                                           'longitude': -118.3800862})
         self.assertEqual(200, response.status_code)
+        self.assertEqual(2, response.json()['count'])
         self.assertEqual('Spring Street Community Garden',
                          response.json()['results'][0]['name'])
 
-    def test_fetches_by_location_with_small_radius_override(self):
+    def test_location_with_small_radius_override(self):
         # Staples Center
         response = self.client.get('/nearby_locations/', {'radius': 1,
                                                           'latitude': 34.0371399,
@@ -57,18 +59,32 @@ class TestViews(TestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual([], response.json()['results'])
 
-    def test_fetches_by_location_with_no_lat_long(self):
+    def test_location_with_no_lat_long(self):
         response = self.client.get('/nearby_locations/')
         self.assertEqual(200, response.status_code)
         self.assertEqual([], response.json()['results'])
 
-    def test_fetches_community_gardens(self):
+    def test_location_detail(self):
+        locations = self.client.get('/locations/')
+        id = locations.json()['results'][0]['id']
+        response = self.client.get('/locations/{}/'.format(id))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('Spring Street Community Garden',
+                         response.json()['name'])
+
+    def test_location_detail_no_id_found(self):
+        response = self.client.get('/locations/10000000000')
+        self.assertEqual(301, response.status_code)
+
+    def test_community_gardens(self):
         response = self.client.get('/locations/community_gardens/')
         self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.json()['count'])
         self.assertEqual('Spring Street Community Garden',
                          response.json()['results'][0]['name'])
 
-    def test_fetches_grocery_stores(self):
+    def test_grocery_stores(self):
         response = self.client.get('/locations/grocery_stores/')
         self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.json()['count'])
         self.assertEqual("Trader Joe's", response.json()['results'][0]['name'])
